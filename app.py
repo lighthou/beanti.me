3a42eddd580d813746bbb752ec0acd0b400041c9
Jeadie <jack.eadie0@gmail.com>
2018-04-21 09:57:49 +1000

    introduce react

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ database = DBConnection()
 
 @app.get('/')
 def index():
-    return static_file("index.html")
+    return static_file("index.html", root= "beanweb/public/")
 
 if __name__ == '__main__':
     run(app, host="localhost", port=8080)
